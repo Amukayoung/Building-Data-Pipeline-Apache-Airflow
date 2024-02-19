9e2ab27614c21d5b45fe639b221c2e9240acccc1
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2024-02-19 23:32:36 +0300

    Added data extraction DAG
    Created a DAG that can extract data from a file, clean it and make post request to an API

---
+++ dags/danalysis_dag.py
@@ -0,0 +1,120 @@
+from datetime import datetime, timedelta
+import json
+import os
+import requests
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+import os
+
+dag_directory = os.path.dirname(os.path.abspath(__file__))
+
+json_file_path = os.path.join(dag_directory, "jsonData/preferences.json")
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 2, 1),
+    'email': 'amukayoung@gmail.com',
+    'catchup':False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def load_data():
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+def extract_data(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(task_ids='load_data_task')
+
+    deviceSet = set()
+    deviceList = []
+
+    for item in data:
+        device_id = item.get("deviceId")
+        app_version = item.get("appVersion", "STD001")
+        device_model = item.get("deviceModel")
+        os = item.get("os")
+        os_version = item.get("osVersion")
+
+        if device_id not in deviceSet:
+            deviceDetail = {
+                'id': device_id,
+                'appversion': app_version,
+                'devicemodel': device_model,
+                'os': os,
+                'osversion': os_version,
+            }
+
+            deviceSet.add(device_id)
+            deviceList.append(deviceDetail)
+
+    ti.xcom_push(key='deviceList', value=deviceList)
+
+    return deviceList
+
+def make_post_request(**kwargs):
+    ti = kwargs['ti']
+    deviceList = ti.xcom_pull(task_ids='extract_data_task', key='deviceList')
+
+    api_endpoint = "http://localhost:8000/device_airflow/"
+
+    headers = {
+        'Content-Type': 'application/json', 
+        'Accept':'application/json',
+        'Content-Length':'<calculated when request is sent>',
+        'Host':'<calculated when request is sent>',
+        # 'User-Agent':'PostmanRuntime/7.36.1',
+        'Accept':'*/*',
+        'Accept-Encoding':'gzip, deflate, br',
+        'Connection':'keep-alive'
+    }
+
+    item={
+        "id":"8590f9e8-a466-4e5f-9397",
+        "appversion":"200104",
+        "devicemodel":"SM-G973F",
+        "os":"android",
+        "osversion":"11"
+    }
+    # for item in deviceList:
+    response = requests.post(api_endpoint, json=item)
+        # logging.info(f"POST request response for item {item}: {response.text}")
+
+dag = DAG(
+    'Danalysis_json_processing_dag',
+    default_args=default_args,
+    description='DAG to process data from JSON file and make a POST request',
+    schedule_interval='@daily',  
+)
+
+task_load_data = PythonOperator(
+    task_id='load_data_task',
+    python_callable=load_data,
+    dag=dag,
+)
+
+task_extract_data = PythonOperator(
+    task_id='extract_data_task',
+    python_callable=extract_data,
+    provide_context=True,
+    dag=dag,
+)
+
+task_make_post_request = PythonOperator(
+    task_id='make_post_request_task',
+    python_callable=make_post_request,
+    provide_context=True,
+    dag=dag,
+)
+
+task_load_data >> task_extract_data >> task_make_post_request
+
+if __name__ == "__main__":
+    dag.cli()
